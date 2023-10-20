acfba484213827c3a7a20aa6c1098ade938a6c72
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-10-20 17:28:30 -0300

    connect video streams

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ io.on('connection', socket => {
         console.log('join-room', roomId, userId)
         socket.join(roomId)
         socket.to(roomId).emit('user-connected', userId)
+
+        socket.on('disconnect', () => {
+            socket.to(roomId.emit('user-disconnected', userId))
+        })
     })
 })
 
