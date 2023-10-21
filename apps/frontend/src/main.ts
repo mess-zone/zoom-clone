d8f6b00a0719a2e6a96d9ede1165b8931ea720d6
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-10-21 18:56:37 -0300

    refactor fontawesome config file

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -2,18 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './routes'
-
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-/* import specific icons */
-import { faVideo, faVideoSlash, faMicrophone, faMicrophoneSlash, faEllipsisVertical, faXmark, faPhone } from '@fortawesome/free-solid-svg-icons'
-
-/* add icons to the library */
-library.add(faVideo, faVideoSlash, faMicrophone, faMicrophoneSlash, faEllipsisVertical, faXmark, faPhone)
+import FontAwesomeIcon from "./config/font-awesome";
 
 const app = createApp(App)
 app.use(router)
