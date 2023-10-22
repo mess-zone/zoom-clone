c191854d68410171ab33d1f6d398422fc2733da1
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-10-21 22:12:17 -0300

    created SettingsModal component

--- apps/frontend/src/views/RoomView.vue
+++ apps/frontend/src/views/RoomView.vue
@@ -66,47 +66,7 @@
         </div>
 
         <Teleport to="body">
-            <Modal :isOpen="settingsModalIsOpen" @close="closeSettingsModal" >
-                <template v-slot:title>
-                    Settings
-                </template>
-                <div class="config-group">
-                    <h3>Video</h3>
-                    <h4>Camera</h4>
-                    <ul class="select-list">
-                        <li
-                            v-for="camera of cameras"
-                            :key="camera.deviceId"
-                            class="option"
-                            :class="{
-                                'active': currentCamera === camera.deviceId,
-                            }"
-                            @click="currentCamera = camera.deviceId"
-                        >
-                            {{ camera.label }}
-                        </li>
-                    </ul>
-                </div>
-                <div class="config-group">
-                    <h3>Audio</h3>
-                    <h4>Microfone</h4>
-
-                    <ul class="select-list">
-                        <li
-                            v-for="microfone of microphones"
-                            :key="microfone.deviceId"
-                            class="option"
-                            :class="{
-                                'active':
-                                    currentMicrophone === microfone.deviceId,
-                            }"
-                            @click="currentMicrophone = microfone.deviceId"
-                        >
-                            {{ microfone.label }}
-                        </li>
-                    </ul>
-                </div>
-            </Modal>
+            <SettingsModal :isOpen="settingsModalIsOpen" @close="closeSettingsModal" />
         </Teleport>
     </div>
 </template>
@@ -123,7 +83,7 @@ import { useUserMedia } from "@vueuse/core";
 import { Peer } from "peerjs";
 import throttle from "lodash.throttle";
 import router from "../routes";
-import Modal from "../components/Modal.vue";
+import SettingsModal from "../components/organisms/SettingsModal.vue";
 
 const route = useRoute();
 
@@ -142,9 +102,7 @@ function disconnect() {
 
 const { 
     currentCamera, 
-    currentMicrophone, 
-    cameras, 
-    microphones,
+    currentMicrophone
 } = useDevices()
 
 const video = ref<HTMLVideoElement>();
@@ -418,36 +376,5 @@ function handleLeaveRoom() {
     justify-content: center;
     align-items: center;
     gap: 10px;
-}
-
-
-/**
-Settings modal content
-
-*/
-
-.config-group {
-    border-top: 1px solid rgba(0, 0, 0, 0.1);
-    padding: 15px 0;
-}
-
-.select-list {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-}
-
-.select-list li {
-    padding: 5px 10px;
-    cursor: pointer;
-}
-
-.select-list li:hover {
-    background-color: rgba(0, 0, 0, 0.1);
-}
-
-.select-list li.active {
-    color: green;
-    font-weight: bold;
 }
 </style>
